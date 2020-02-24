cee223fdb3e604c118116456e1aa02378403e322
qutl <qutl@nextop.cn>
2020-02-24 11:02:57 +0800

    z

--- src/main/java/cn/nextop/guava/widgets/datetime/XDateTimePopup.java
+++ src/main/java/cn/nextop/guava/widgets/datetime/XDateTimePopup.java
@@ -4,9 +4,14 @@ import static cn.nextop.guava.utils.SwtUtils.shell;
 
 import org.eclipse.draw2d.LightweightSystem;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
 
 import cn.nextop.guava.widgets.datetime.render.popup.PopupPanel;
 
@@ -15,6 +20,7 @@ import cn.nextop.guava.widgets.datetime.render.popup.PopupPanel;
  */
 public class XDateTimePopup extends Canvas {
 	//
+	private Layout layout;
 	private XDateTime dateTime;
 	private LightweightSystem lws;
 	
@@ -32,12 +38,42 @@ public class XDateTimePopup extends Canvas {
 	}
 
 	public void show() {
-		this.dateTime.getModel().getLayout().layout(dateTime); getShell().open();
+		layout.layout(dateTime); getShell().open();
 	}
 	
 	public void hide() {
 		this.dateTime.setPopup(null); this.dateTime.redraw(); getShell().dispose();
 	}
 	
+	/**
+	 * 
+	 */
 	private class DeactivateListener implements Listener { @Override public void handleEvent(Event event) { hide(); }}
+	
+	/**
+	 * 
+	 */
+	private class Layout {
+		/**
+		 * 
+		 */
+		public void layout(XDateTime time) {
+			XDateTimePopup popup = time.getPopup();
+			final Shell shell = popup.getShell();
+			final Composite parent = time.getParent();
+			final Rectangle bounds = time.getBounds();
+			final Display display = shell.getDisplay();
+			final Rectangle r1 = display.map(parent, null, bounds);
+			final Rectangle r2 = shell.getMonitor().getClientArea();
+			// bound
+			shell.setSize(300, 350);
+			// location
+			final int margin = 2;
+			final Point size = shell.getSize();
+			int x = r1.x, y = r1.y + r1.height + margin;
+			if(y + size.y > r2.y + r2.height) y = r1.y - size.y - margin;
+			if(x < r2.x) x = r2.x; else if(x + size.x > r2.x + r2.width) x = r2.x + r2.width - size.x;
+			shell.setLocation(x, y);
+		}
+	}
 }
--- src/main/java/cn/nextop/guava/widgets/datetime/model/XDateTimeModel.java
+++ src/main/java/cn/nextop/guava/widgets/datetime/model/XDateTimeModel.java
@@ -3,11 +3,7 @@ package cn.nextop.guava.widgets.datetime.model;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import cn.nextop.guava.widgets.datetime.layout.ShellLayout;
-
 public class XDateTimeModel {
-	//
-	private ShellLayout layout;
 	//
 	private long time;
 	private SimpleDateFormat formater_1;
@@ -16,21 +12,12 @@ public class XDateTimeModel {
 	 * 
 	 */
 	public XDateTimeModel() {
-		layout = new ShellLayout();
 		formater_1 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 	}
 	
 	/**
 	 * 
 	 */
-	public ShellLayout getLayout() {
-		return layout;
-	}
-
-	public void setLayout(ShellLayout layout) {
-		this.layout = layout;
-	}
-	
 	public String getTime() {
 		return formater_1.format(new Date(time));
 	}
