dc8dfaf45ff5bd63eefacd789f67d1d057f86c09
qutl <qutl@nextop.cn>
2020-04-01 14:38:16 +0800

    z

--- src/main/java/cn/nextop/guava/Example.java
+++ src/main/java/cn/nextop/guava/Example.java
@@ -35,24 +35,24 @@ public class Example {
 		final String name = "Widget Example";
 		Shell shell = creator(500, 500, name);
 		Composite cmp = SwtUtils.creator(shell);
-		cmp.setLayout(new MigLayout("insets 5, gap 0 0","[fill,grow,center][30][fill,grow,center][30]","[fill,grow][fill,grow][fill,grow][fill,grow][fill,grow][fill,grow]"));
+		cmp.setLayout(new MigLayout("insets 5, gap 0 0","[fill,grow,center][fill,grow,center]","[fill,grow][fill,grow][fill,grow][fill,grow][fill,grow][fill,grow][fill,grow]"));
 		//datetime
-		XDateTime date = new XDateTime(cmp); date.setLayoutData("cell 0 0, width 10:150:,height 23!, span 4"); date.setInput(System.currentTimeMillis());
+		XDateTime date = new XDateTime(cmp); date.setLayoutData("cell 0 0, width 10:150:,height 23!, span 2"); date.setInput(System.currentTimeMillis());
 		//spinner 1
 		XSpinner<BigDecimal> spinner1 = new XSpinner<>(cmp, SWT.VERTICAL, new BigDecimalFormatter("##0.00","##0")); 
-		spinner1.setLayoutData("cell 0 1, width 10:150:,height 23!, span 4"); 
+		spinner1.setLayoutData("cell 0 1, width 10:150:,height 23!, span 2"); 
 		spinner1.setValue(new BigDecimal("100.00"), new BigDecimal("-100.00"), new BigDecimal("0.01"), new BigDecimal("1.00"));
 		//spinner 2
 		XSpinner<Long> spinner2 = new XSpinner<>(cmp, SWT.HORIZONTAL, new LongFormatter("-########0","########0")); 
-		spinner2.setLayoutData("cell 0 2, width 10:150:,height 50!, span 4"); 
+		spinner2.setLayoutData("cell 0 2, width 10:150:,height 50!, span 2"); 
 		spinner2.setValue(new Long(999999999), new Long(-999999999), new Long(100), new Long(100));
 		//slider
-		XSlider slider = new XSlider(cmp, SWT.VERTICAL); slider.setLayoutData("cell 0 3, width 10:150:,height 24!, span 4");
+		XSlider slider = new XSlider(cmp, SWT.VERTICAL); slider.setLayoutData("cell 0 3, width 10:150:,height 24!, span 2");
 		slider.setValue(0, 100, 23);
 		//circle progress
 		XCircleProgress progress = new XCircleProgress(cmp); progress.setLayoutData("cell 0 4, width 100!,height 100!");
 		progress.init(0f, 100f, 25f);
-		Button btnOk1 = new Button(cmp, SWT.NONE); btnOk1.setLayoutData("cell 1 4, height 30!, bottom");
+		Button btnOk1 = new Button(cmp, SWT.NONE); btnOk1.setLayoutData("cell 0 5, height 30!, top");
 		btnOk1.setText("Start Progress"); btnOk1.addSelectionListener(new SelectionAdapter() {
 			boolean finished = false; @Override public void widgetSelected(SelectionEvent e) {
 				super.widgetSelected(e); if(finished) return;
@@ -64,14 +64,14 @@ public class Example {
 			}
 		});
 		//infinite progress
-		XInfiniteProgress progress1 = new XInfiniteProgress(cmp); progress1.setLayoutData("cell 2 4, width 100!,height 100!");
-		Button btnOk2 = new Button(cmp, SWT.NONE); btnOk2.setLayoutData("cell 3 4, height 30!, bottom");
+		XInfiniteProgress progress1 = new XInfiniteProgress(cmp); progress1.setLayoutData("cell 1 4, width 100!,height 100!");
+		Button btnOk2 = new Button(cmp, SWT.NONE); btnOk2.setLayoutData("cell 1 5, height 30!, top");
 		btnOk2.setText("Start Progress"); btnOk2.addSelectionListener(new SelectionAdapter() {
 			boolean running = false; @Override public void widgetSelected(SelectionEvent e) {
 				super.widgetSelected(e); if (!running) {
-					running = true;	progress1.start(); btnOk2.setText("Start Progress");
+					running = true;	progress1.start(); btnOk2.setText("Stop Progress");
 				} else {
-					running = false; progress1.stop(); btnOk2.setText("Stop Progress");
+					running = false; progress1.stop(); btnOk2.setText("Start Progress");
 				}
 					
 			}
